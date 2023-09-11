6cde776f1fbc64b8e431dfec101156be3e1a737a
Josh Johnston <josh@x-team.com>
2023-09-11 11:36:29 +1000

    allow node >= 14 across all packages

--- examples/getstarted/config/database.js
+++ examples/getstarted/config/database.js
@@ -1,48 +1,11 @@
-const sqlite = {
-  connector: 'bookshelf',
-  settings: {
-    client: 'sqlite',
-    filename: '.tmp/data.db',
-  },
-  options: {
-    // debug: true,
-    useNullAsDefault: true,
-  },
-};
-
-const postgres = {
-  connector: 'bookshelf',
-  settings: {
-    client: 'postgres',
-    database: 'strapi',
-    username: 'strapi',
-    password: 'strapi',
-    port: 5432,
-    host: 'localhost',
-  },
-  options: {},
-};
-
 const mysql = {
   connector: 'bookshelf',
   settings: {
     client: 'mysql',
     database: 'strapi',
-    username: 'strapi',
-    password: 'strapi',
-    port: 3306,
-    host: 'localhost',
-  },
-  options: {},
-};
-
-const mongo = {
-  connector: 'mongoose',
-  settings: {
-    database: 'strapi',
     username: 'root',
-    password: 'strapi',
-    port: 27017,
+    password: 'admin123',
+    port: 3308,
     host: 'localhost',
   },
   options: {},
@@ -50,14 +13,11 @@ const mongo = {
 
 const db = {
   mysql,
-  sqlite,
-  postgres,
-  mongo,
 };
 
 module.exports = {
   defaultConnection: 'default',
   connections: {
-    default: process.env.DB ? db[process.env.DB] || db.sqlite : db.sqlite,
+    default: process.env.DB ? db[process.env.DB] || db.mysql : db.mysql,
   },
 };
